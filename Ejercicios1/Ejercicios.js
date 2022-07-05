1525226ccf2b83672e4458680e9ecbe1ba501ee6
spinon1 <spinon@corpomk.com.mx>
2022-07-05 10:41:48 -0600

    Ejecicios 1

---
+++ Ejercicios1/Ejercicios.js
@@ -0,0 +1,21 @@
+// Ejercicio 1 
+xnumero=prompt("Digite un Numero:");
+xnumero=xnumero*2;
+console.log(xnumero);
+
+// Ejercicio 2
+xnumero1=20;
+xnumero2=10;
+xresultado=xnumero1-xnumero2;
+console.log(xresultado);
+
+// Ejercicio 3
+r=5.3;
+xarea_circulo=Math.PI*(r**2);
+console.log(xarea_circulo);
+
+// Ejercicio 4
+D=8;
+d=6;
+xarea_rombo=(D*d)/2;
+console.log(xarea_rombo);
