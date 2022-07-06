738e02d217b03b4da2eec1e3a72389c28c92ca81
spinon1 <spinon@corpomk.com.mx>
2022-07-06 16:58:23 -0600

    actualizacion ejecicio2

--- Ejercicios2/Ejercicio.js
+++ Ejercicios2/Ejercicio.js
@@ -1,11 +1,11 @@
 let xedad=18;
 let xfrontera=false;
 let xembarazo=true;
-let xsem_gestacion=8;
+let xsem_gestacion=9;
 let xano_nac=1971;
 let currentTime = new Date();
 
-if ((currentTime.getFullYear()-xano_nac)>=30) {
+if ((currentTime.getFullYear()-xano_nac)>=30 && !(xembarazo)) {
     console.log("Mayor a 30 años o cumple");
 } else if (xedad>=18 && xfrontera) {
     console.log("Mayor a 18 y es frontera");
