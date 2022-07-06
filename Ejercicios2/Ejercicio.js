fb40dd962e28ffb128b2524603f61700ab2bc981
spinon1 <spinon@corpomk.com.mx>
2022-07-06 16:36:56 -0600

    Ejecicio2 añadido

---
+++ Ejercicios2/Ejercicio.js
@@ -0,0 +1,16 @@
+let xedad=18;
+let xfrontera=false;
+let xembarazo=true;
+let xsem_gestacion=8;
+let xano_nac=1971;
+let currentTime = new Date();
+
+if ((currentTime.getFullYear()-xano_nac)>=30) {
+    console.log("Mayor a 30 años o cumple");
+} else if (xedad>=18 && xfrontera) {
+    console.log("Mayor a 18 y es frontera");
+} else if (xembarazo && xsem_gestacion>=9){
+    console.log("embarazo y mayor de 9 semanas")
+} else {
+    console.log("No se puede vacunar")
+}
